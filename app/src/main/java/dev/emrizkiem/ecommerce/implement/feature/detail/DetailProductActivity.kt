aeb545610ed318ac735d0f914e5d1ec066ee6b7c
Rizki Maulana <mr.hobskoy@gmail.com>
2021-07-15 23:45:08 +0700

    Add component for layout detail product and change button to material component

---
+++ app/src/main/java/dev/emrizkiem/ecommerce/implement/feature/detail/DetailProductActivity.kt
@@ -0,0 +1,12 @@
+package dev.emrizkiem.ecommerce.implement.feature.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.emrizkiem.ecommerce.R
+
+class DetailProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_product)
+    }
+}
--- app/src/main/java/dev/emrizkiem/ecommerce/implement/feature/home/HomeActivity.kt
+++ app/src/main/java/dev/emrizkiem/ecommerce/implement/feature/home/HomeActivity.kt
@@ -1,10 +1,13 @@
 package dev.emrizkiem.ecommerce.implement.feature.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
 import dev.emrizkiem.ecommerce.R
+import dev.emrizkiem.ecommerce.implement.feature.detail.DetailProductActivity
 
 class HomeActivity : AppCompatActivity() {
 
@@ -15,6 +18,12 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val recyclerView: RecyclerView = findViewById(R.id.rv_product)
+        val imgLiveRoom: ShapeableImageView = findViewById(R.id.ic_live_room)
+
+        imgLiveRoom.setOnClickListener {
+            val intent = Intent(this, DetailProductActivity::class.java)
+            startActivity(intent)
+        }
 
         recyclerView.apply {
             homeAdapter = HomeAdapter()
