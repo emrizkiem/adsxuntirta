dcaece79ae7c0be9665d23c61161e21496ad92fa
Rizki Maulana <mr.hobskoy@gmail.com>
2021-07-22 22:02:57 +0700

    Integration category and product with firebase

--- app/src/main/java/dev/emrizkiem/ecommerce/implement/feature/home/CategoryAdapter.kt
+++ app/src/main/java/dev/emrizkiem/ecommerce/implement/feature/home/CategoryAdapter.kt
@@ -3,7 +3,10 @@ package dev.emrizkiem.ecommerce.implement.feature.home
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.material.imageview.ShapeableImageView
 import dev.emrizkiem.ecommerce.R
 import dev.emrizkiem.ecommerce.implement.data.Category
 
@@ -11,16 +14,25 @@ class CategoryAdapter(
     private val listData: List<Category>
 ) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        // Todo
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val imgCategory: ShapeableImageView = view.findViewById(R.id.img_category)
+        private val tvCategory: TextView = view.findViewById(R.id.tv_category)
+
+        fun bindItem(category: Category) {
+            tvCategory.text = category.name
+
+            Glide.with(itemView.context)
+                .load(category.image)
+                .into(imgCategory)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_product, parent, false))
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // Todo
+        holder.bindItem(listData[position])
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/dev/emrizkiem/ecommerce/implement/feature/home/HomeActivity.kt
+++ app/src/main/java/dev/emrizkiem/ecommerce/implement/feature/home/HomeActivity.kt
@@ -3,13 +3,14 @@ package dev.emrizkiem.ecommerce.implement.feature.home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import dev.emrizkiem.ecommerce.R
+import dev.emrizkiem.ecommerce.implement.data.Category
 import dev.emrizkiem.ecommerce.implement.data.Product
 import dev.emrizkiem.ecommerce.implement.feature.detail.DetailProductActivity
 import dev.emrizkiem.ecommerce.implement.feature.login.LoginActivity
@@ -17,13 +18,16 @@ import dev.emrizkiem.ecommerce.implement.feature.login.LoginActivity
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var productAdapter: ProductAdapter
+    private lateinit var categoryAdapter: CategoryAdapter
 
     private lateinit var auth: FirebaseAuth
     private lateinit var dbReference: DatabaseReference
     private lateinit var dbInstance: FirebaseDatabase
 
     private lateinit var listData: ArrayList<Product>
+    private lateinit var listDataCategory: ArrayList<Category>
 
+    private lateinit var rvCategory: RecyclerView
     private lateinit var rvProduct: RecyclerView
     private lateinit var logOut: ImageView
 
@@ -31,6 +35,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        rvCategory = findViewById(R.id.rv_category)
         rvProduct = findViewById(R.id.rv_product)
         logOut = findViewById(R.id.ic_logout)
 
@@ -45,11 +50,37 @@ class HomeActivity : AppCompatActivity() {
 //            Log.d("Check User", "Already logged in")
 //        }
 
-        getDataFromFirebase()
+        getDataCategoryFromFirebase()
+        getDataProductFromFirebase()
         goToLogout()
     }
 
-    private fun getDataFromFirebase() {
+    private fun getDataCategoryFromFirebase() {
+        listDataCategory = ArrayList()
+        categoryAdapter = CategoryAdapter(listDataCategory)
+
+        rvCategory.layoutManager = GridLayoutManager(this, 2)
+        rvCategory.setHasFixedSize(true)
+
+        dbReference = dbInstance.getReference("category")
+        dbReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (data in snapshot.children) {
+                        val item = data.getValue(Category::class.java)
+                        item?.let { listDataCategory.add(it) }
+                    }
+                    rvCategory.adapter = categoryAdapter
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
+    }
+
+    private fun getDataProductFromFirebase() {
         listData = ArrayList()
         productAdapter = ProductAdapter(listData) {
             startActivity(Intent(this@HomeActivity, DetailProductActivity::class.java))
